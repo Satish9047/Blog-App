101b1e07c7598918bb9957b4e49c15539587a5e6
Satish9047 <satishprajapati930@gmail.com>
2023-09-16 21:38:56 +0545

    addded react-quill and creat new post

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,6 +1,16 @@
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div>
+      <div>Create New Post</div>
+      <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Summary"/>
+      <input type="file"/>
+      <ReactQuill theme="snow"/>;
+      
+    </div>
   )
 }
 export default CreatePost
