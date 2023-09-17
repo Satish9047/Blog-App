1559facffb9161df56f8913a2220419ba03cf195
Satish9047 <satishprajapati930@gmail.com>
2023-09-17 21:48:04 +0545

    update state in create new post

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,15 +1,25 @@
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
+import { useState } from "react";
 
 const CreatePost = () => {
+  const [title, setTitle]=useState("");
+  const [summary, setSummary]=useState("")
+
+  const handleCreatePost = (e)=>{
+    e.preventDefault();
+    
+  }
   return (
     <div>
-      <div>Create New Post</div>
-      <input type="text" placeholder="Title" />
-      <input type="text" placeholder="Summary"/>
-      <input type="file"/>
-      <ReactQuill theme="snow"/>;
-      
+      <form onSubmit={handleCreatePost}>
+        <div>Create New Post</div>
+        <input type="text" placeholder="Title" value={title} onChange={(ev)=>{setTitle(ev.target.value)}}/>
+        <input type="text" placeholder="Summary" value={summary} onChange={(ev)=>setSummary(ev.target.value)}/>
+        <input type="file"/>
+        <ReactQuill theme="snow"/>;
+        <button type="submit">Create</button>
+      </form>
     </div>
   )
 }
