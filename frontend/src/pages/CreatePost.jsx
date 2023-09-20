93fda08ec73f0576111e76b1787e1b96df0d3c86
Satish9047 <satishprajapati930@gmail.com>
2023-09-20 21:26:07 +0545

    added create blog request

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -4,20 +4,42 @@ import { useState } from "react";
 
 const CreatePost = () => {
   const [title, setTitle]=useState("");
-  const [summary, setSummary]=useState("")
+  const [summary, setSummary]=useState("");
+  const [blog, setBlog]=useState("");
 
-  const handleCreatePost = (e)=>{
+  const handleCreatePost = async(e)=>{
     e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/createBlogPost", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          title,
+          summary,
+          blog
+        })
+      });
+
+      if(response.status == 200){
+        const data = await response.json();
+        console.log(data);
+      }
+      
+    } catch (error) {
+      console.error(error);
+    }
     
   }
   return (
     <div>
-      <form onSubmit={handleCreatePost}>
+      <form method="post" encType="multipart/form-data" onSubmit={handleCreatePost} >
         <div>Create New Post</div>
         <input type="text" placeholder="Title" value={title} onChange={(ev)=>{setTitle(ev.target.value)}}/>
         <input type="text" placeholder="Summary" value={summary} onChange={(ev)=>setSummary(ev.target.value)}/>
         <input type="file"/>
-        <ReactQuill theme="snow"/>;
+        <ReactQuill theme="snow" value={blog} onChange={(ev)=>setBlog(ev.target.value)}/>;
         <button type="submit">Create</button>
       </form>
     </div>
--- server/src/controllers/createPost.controller.js
+++ server/src/controllers/createPost.controller.js
@@ -1,6 +1,7 @@
 
 const createBlogPostController = (req, res)=>{
-    console,log(req.body);
+    console.log(req.file, req.body);
+    return res.status(200).json({success: "this is createBlogPost Controller"});
 }
 
 module.exports= {
